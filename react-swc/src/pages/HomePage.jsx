36b6ca3fdadea7b1007a1863fa0f0df3e2a1c702
Kenji Phang <ken.pcx@outlook.com>
2023-08-04 13:46:34 +0100

    Added countdown and home page

---
+++ react-swc/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Countdown from "../components/Countdown";
+
+const HomePage = () => {
+  return (
+    <>
+      <Countdown targetDate={"2023-10-10"} />
+    </>
+  );
+};
+
+export default HomePage;
