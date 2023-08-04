e7c2d7aaddcb4439cedb02c842af7d01aa81b493
Kenji Phang <ken.pcx@outlook.com>
2023-08-04 15:58:25 +0100

    Added search bar

--- react-swc/src/pages/EventsPage.jsx
+++ react-swc/src/pages/EventsPage.jsx
@@ -3,6 +3,13 @@ import Box from "@mui/material/Box";
 import EventCard from "../components/events/EventCard";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Unstable_Grid2";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
+import { styled, alpha } from "@mui/material/styles";
+import { useRef } from "react";
+import { useEffect } from "react";
 
 const eventsData = [
   {
@@ -183,10 +190,71 @@ const eventsData = [
     lms_link: "https://example.com/lms/web-dev-workshop",
   },
 ];
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
 
 const EventsPage = () => {
+  const searchRef = useRef(null);
+
+  const handleSearch = () => {
+    console.log(searchRef.current.value);
+  };
+
   return (
     <Box>
+      <Search>
+        <SearchIconWrapper>
+          <SearchIcon />
+        </SearchIconWrapper>
+        <StyledInputBase
+          placeholder="Search…"
+          inputProps={{ "aria-label": "search" }}
+          inputRef={searchRef}
+          onKeyDown={(e) => {
+            if (e.key.toLowerCase() === "enter") {
+              handleSearch();
+            }
+          }}
+        />
+      </Search>
       <Grid container spacing={2} columns={3} columnGap={3} rowGap={3}>
         {eventsData.map((event) => (
           <EventCard
